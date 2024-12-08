74642a944bc15422846f0082ebdc5f1641f38c09
德华 刘 <gavinprior5@gmail.com>
2024-12-08 12:12:39 +0000

    rename file

---
+++ components/ui/header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <header className="w-full h-24 sticky top-0 shadow-md bg-background">
+      <nav>
+        <Link href="/">Coco Cleaning Co.</Link>
+        <ul>
+            
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavigationBar;
