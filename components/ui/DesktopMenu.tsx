fa02ac9ce2a557fd8951093f566a6e12fc3c0684
德华 刘 <gavinprior5@gmail.com>
2024-12-16 15:15:06 +0000

    add pricing link

--- components/ui/DesktopMenu.tsx
+++ components/ui/DesktopMenu.tsx
@@ -11,6 +11,7 @@ const navLinks = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
+  { name: "Pricing", href: "/pricing" },
 ];
 
 const DesktopMenu = () => {
